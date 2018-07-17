083b101ea68fad32a83156e9883b26190b6e13fd
Eli Judah <elijudah@gmail.com>
2018-07-17 14:27:18 +0100

    Formatting

--- algorithms/bubblesort/bubble_sort.c
+++ algorithms/bubblesort/bubble_sort.c
@@ -2,7 +2,7 @@
 
 int bubbleSort(int arr[], int size) {
     int i, j, swap;
-    for (i = 0; i < size; i++){
+    for (i = 0; i < size; i++) {
         for (j = 0; j < size - 1; j++) {
             if (arr[j] > arr[j+1]) {
                 swap = arr[j];
@@ -19,7 +19,7 @@ int bubbleSort(int arr[], int size) {
 }
 
 int main() {
-    int unsorted[] = {9, 2, 8, 3, 1, 10, 6, 7, 4, 5,};
+    int unsorted[] = {9, 2, 8, 3, 1, 10, 6, 7, 4, 5};
     size_t size = sizeof(unsorted) / sizeof(unsorted[0]);
     bubbleSort(unsorted, size);
 
